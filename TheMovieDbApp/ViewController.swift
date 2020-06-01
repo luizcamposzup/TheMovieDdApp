e3bf70e471a8876deca8e629e8a3df98eed49405
Luiz Henrique Carminati Campos <luiz.campos@zup.com.br>
2020-06-01 11:47:19 -0300

    add base xcode project for initialize the project

---
+++ TheMovieDbApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheMovieDbApp
+//
+//  Created by zupper on 01/06/20.
+//  Copyright © 2020 Luizhcarminati. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
